5fc2f4533bcc96d05bb90a22ed90a23239478d90
AnirudhChidanand <anirudh.mc.hegde@gmail.com>
2025-08-29 13:47:28 +0530

    root msg

--- app/main.py
+++ app/main.py
@@ -22,3 +22,8 @@ app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.get('/')
+def root():
+    return {"message": "Hello world!"}
